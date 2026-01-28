9bfee4a3860d91634727dd61f49295eda3e26567
Ruslan Antokhin <rusantokhin@gmail.com>
2026-01-28 16:47:53 +0300

    First commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+	description = "NixOS flake";
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = { self, nixpkgs, home-manager, ... }: {
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				./configuration.nix
+				home-manager.nixosModules.home-manager
+				{
+					home-manager = {
+						useGlobalPkgs = true;
+						useUserPackages = true;
+						users.rusantokhin = import ./home.nix;
+						backupFileExtension = "backup";
+					};
+				}
+			];
+		};
+	};
+}
---
+++ home.nix
@@ -0,0 +1,63 @@
+{ config, pkgs, ... }:
+let
+	dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+	create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+	configs = {
+		qtile = "qtile";
+		alacritty = "alacritty";
+		rofi = "rofi";	
+	};
+in
+
+{
+	home = {
+		username = "rusantokhin";
+		homeDirectory = "/home/rusantokhin";
+		stateVersion = "26.05";
+	};
+
+	xdg.configFile = builtins.mapAttrs (name: subpath: {
+		source = create_symlink "${dotfiles}/${subpath}";
+		recursive = true;
+	})
+	configs;
+	
+	#xdg.configFile = {
+	#	"qtile" = {
+	#		source = create_symlink "${dotfiles}/qtile/";
+	#		recursive = true;
+	#	};	
+	#	"alacritty" = {
+	#		source = create_symlink "${dotfiles}/alacritty";
+	#		recursive = true;
+	#	};
+	#};
+
+	programs = {
+	
+		git = {
+			enable = true;
+			settings = {
+				user = {
+					name = "Ruslan Antokhin";
+					email = "rusantokhin@gmail.com";
+				};
+				init.defaultBranch = "main";
+			};
+		};
+		
+		bash = {
+			enable = true;
+			shellAliases = {
+				btw = "echo I use NixOS, btw";
+			};
+		};
+		
+	};
+	fonts.fontconfig.enable = true;
+	home.packages = with pkgs; [
+		alacritty
+		rofi
+		nerd-fonts.jetbrains-mono
+	];
+}
